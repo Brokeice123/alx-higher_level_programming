eb813ea1fc4199787843f990c1ae978309b6a06c
Brokeice123 <keithmarkomen@gmail.com>
2023-10-05 16:04:00 +0300

    a program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -18,3 +18,4 @@ if __name__ == "__main__":
         print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
