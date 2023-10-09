da83026def5d8d3a616b3ae939645c71b4810d1a
Brokeice123 <keithmarkomen@gmail.com>
2023-10-09 13:49:10 +0300

    a C function that prints some basic info about Python lists.

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,39 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <python.h>
-
 /**
- * print_python_list_info - prints info about python list
- * @p: pointer to python list
+ * print_python_list_info - Print some basic info about Python lists
+ * @p: PyObject
  *
+ * Return: Nothing
  */
-
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t size;
-	Py_ssize_t i;
-	PyObject *item;
-	Py_ssize_t len;
-	Py_ssize_t index;
+PyObject *item;
+PyListObject *list = (PyListObject *)p;
+int i, size, alloc;
 
-	len = PyList_Size(p);
+size = Py_SIZE(p);
+alloc = list->allocated;
+printf("[*] Size of the Python List = %d\n", size);
+printf("[*] Allocated = %d\n", alloc);
 
-	printf("Python list: [");
-	for (i = 0; i < len; i++)
-	{
-		item = PyList_GetItem(p, i);
-		printf("%s", PyUnicode_AsUTF8(item));
-		if (i != len - 1)
-			printf(", ");
-	}
-	printf("]\n");
-
-	size = PyList_GET_SIZE(p);
-	printf("size: %lu\n", (unsigned long)size);
-
-	index = PyList_GET_SIZE(p);
-	printf("capacity: %lu\n", (unsigned long)index);
-
-	printf("length: %lu\n", (unsigned long)len);
-
+for (i = 0; i < size; i++)
+{
+item =  PyList_GetItem(p, i);
+printf("Element %d: %s\n", i, Py_TYPE(item)->tp_name);
+}
 }
