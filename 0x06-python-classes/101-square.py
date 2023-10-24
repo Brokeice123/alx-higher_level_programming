472597502866d2fb04f36c17222e151fa3728aaa
Brokeice123 <keithmarkomen@gmail.com>
2023-10-24 19:00:33 +0300

    A class Square with Printing printing squae instance working the same as my_print()

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -67,8 +67,8 @@ class Square:
 
     def __str__(self):
         """Return the print() and str() representation of the Square."""
-        if self.__size == 0:
-            return ("")
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
         else:
             [print("") for i in range(0, self.__position[1])]
             for i in range(0, self.__size):
