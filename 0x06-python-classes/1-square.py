3435022d61e0ebe4eef2bc6d0cc3ee20ea3f9c7b
Brokeice123 <keithmarkomen@gmail.com>
2023-10-24 14:05:25 +0300

    A class Square with a Private instance size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""1-square.py"""
+
+
+class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the Square
+    """
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): size of a side of the Square
+        Return: None
+        """
+        self.__size = size
