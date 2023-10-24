7d7b6f2f879b199cd4c5ff65b2a9701c131c12af
Brokeice123 <keithmarkomen@gmail.com>
2023-10-24 23:29:41 +0300

    A class Square that handles comparitors

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (int): size of a side of the Square
+        Return: None
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current area of the Square."""
+
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """Get/set the current size of the Square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __eq__(self, other):
+        """Define the == comparison method.
+        Args:
+            other (Square): The other Square.area to compare to.
+        """
+        return (self.__size == other.__size)
+
+    def __lt__(self, other):
+        """Define the < comparison method.
+        Args:
+            other (Square): The other Square.area to compare to.
+        """
+        return (self.__size < other.__size)
+
+    def __le__(self, other):
+        """Define the <= comparison method.
+        Args:
+            other (Square): The other Square.area to compare to.
+        """
+        return (self.__size <= other.__size)
+
+    def __gt__(self, other):
+        """Define the > comparison method.
+        Args:
+            other (Square): The other Square.area to compare to.
+        """
+        return (self.__size > other.__size)
+
+    def __ge__(self, other):
+        """Define the >= comparison method.
+        Args:
+            other (Square): The other Square.area to compare to.
+        """
+        return (self.__size >= other.__size)
+
+    def __ne__(self, other):
+        """Define the != comparison method.
+        Args:
+            other (Square): The other Square.area to compare to.
+        """
+        return (self.__size != other.__size)
