7899dc0e8479fd1e7558fa772486b768d8436d18
Brokeice123 <keithmarkomen@gmail.com>
2023-10-30 10:24:54 +0300

    a function that prints a text with 2 new lines after standalone special chars, together with it's doctest

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Text indentation module."""
+
+
+def text_indentation(text):
+    """Print text with 2 new lines after each of these characters: ., ? and :."""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for char in ".?:":
+        text = text.replace(char, char + "\n\n")
+    print(text)
