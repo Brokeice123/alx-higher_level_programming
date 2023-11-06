6998234ecbd3aec118f55ea69bcc454c4a9ae600
Brokeice123 <keithmarkomen@gmail.com>
2023-11-06 20:52:31 +0300

    class BaseGeometry with Public instance area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+The class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """A class with public instance methods"""
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
