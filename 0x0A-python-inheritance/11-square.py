96dfe9f12242f131d3ed8012ad559a42802cd662
Brokeice123 <keithmarkomen@gmail.com>
2023-11-07 00:47:02 +0300

    a class Square with __str__

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A class that inherits from Rectangle
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class that inherits from Rectangle"""
+    def __init__(self, size):
+        """Instantiation"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Returns a string representation of the square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
