77fa5afe3beaed7ffcb430f565ff14ec1e719b49
Brokeice123 <keithmarkomen@gmail.com>
2024-01-08 22:00:14 +0000

    Error

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,7 @@ const x = Math.floor(process.argv[2]);
 if (isNaN(x)) {
 	console.log('Missing size');
 } else {
-	for (let r = 0; r < size; r++) {
-	let row = '';
-	for (let c = 0; c < size; c++) row += 'X';
-	console.log(row);
+  for (let i = 0; i < x; i++) {
+    console.log('X'.repeat(x));
   }
 }
