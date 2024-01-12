45bb84a11664d3c17c4b8698808a8cf6d7be59b8
Brokeice123 <keithmarkomen@gmail.com>
2024-01-12 19:51:47 +0000

    a script that takes in arguments and displays all values in the states table, safe from SQL injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A script that takes in an argument and displays all values in the states
+where name matches the argument. Safe from SQL injections.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id"
+                .format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
