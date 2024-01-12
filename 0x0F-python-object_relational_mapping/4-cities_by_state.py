8d085ea7c8aadf2fdc2c119af23326bc6ddbcbcb
Brokeice123 <keithmarkomen@gmail.com>
2024-01-12 20:08:44 +0000

    a script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities " +
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
                 "JOIN states ON cities.state_id = states.id"
                 "ORDER BY cities.id")
     rows = cur.fetchall()
