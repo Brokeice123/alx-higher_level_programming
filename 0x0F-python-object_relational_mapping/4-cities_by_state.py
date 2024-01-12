a0ddb8cc8bc340a184aabd4789ac20ae2bf547c6
Brokeice123 <keithmarkomen@gmail.com>
2024-01-12 20:10:41 +0000

    a script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
-                "JOIN states ON cities.state_id = states.id"
-                "ORDER BY cities.id")
+                "JOIN states ON cities.state_id = states.id "
+                "ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
