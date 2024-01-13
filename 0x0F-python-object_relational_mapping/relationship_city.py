ac7d248f46489132b1c329629368726740b50212
Brokeice123 <keithmarkomen@gmail.com>
2024-01-13 20:52:17 +0000

    Question 100

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -4,9 +4,7 @@ Module that contains the class definition of a City
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from relationship_state import Base
 
 
 class City(Base):
