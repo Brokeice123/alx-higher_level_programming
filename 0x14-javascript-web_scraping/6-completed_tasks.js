558f825dc25bd1df75626ab40bca96c6d9f4e657
Brokeice123 <keithmarkomen@gmail.com>
2024-02-20 19:27:17 +0000

    a script that computes the number of tasks completed by user id.

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        if (completed[todo.userId] === undefined) {
+          completed[todo.userId] = 1;
+        } else {
+          completed[todo.userId] += 1;
+        }
+      }
+    });
+
+    console.log(completed);
+  }
+});
