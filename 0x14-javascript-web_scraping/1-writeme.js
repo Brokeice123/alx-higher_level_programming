9890fe650d6bdf7dd0296942f5de916077869dc8
Brokeice123 <keithmarkomen@gmail.com>
2024-02-20 18:55:26 +0000

    a script that writes a string to a file.

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf8', (error) => {
+  if (error) {
+    console.log(error);
+  }
+});
