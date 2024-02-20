bbe1b9b98e83df76297a452746d10323e1ddc3bf
Brokeice123 <keithmarkomen@gmail.com>
2024-02-20 17:55:29 +0000

    a script that reads and prints the content of a file.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const path = process.argv[2];
+const filePath = process.argv[2];
 
-fs.readFile(path, 'utf8', (err, data) => {
-	if (err) throw err;
-	console.log(data);
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
