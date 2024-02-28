3ac9a04e5b8fb020a337c2959af2b250431abc4a
Brokeice123 <keithmarkomen@gmail.com>
2024-02-28 17:00:52 +0000

    a JavaScript script that updates the text color of the <header> element to red (#FF0000)

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+// Wait for the DOM content to be fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+  // Select the header element
+  const header = document.querySelector('header');
+  // Set the text color of the <header> element to red (#FF0000)
+  header.style.color = '#FF0000';
+});
