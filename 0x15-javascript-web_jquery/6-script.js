e1fcec1b778f04086b3db1e4522d97c45b949ac6
Brokeice123 <keithmarkomen@gmail.com>
2024-02-28 18:17:21 +0000

    a JavaScript script that updates the text of the <header> element to New Headervim 6-script.js testfiles/6-main.html! when the user clicks on DIV#update_header

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   // Add a click event listener to the <div> element with the ID "update_header"
-  $('#update_header').click(function () {
+  $('#DIVupdate_header').click(function () {
     // Update the text of the <header> element to "New Header!!!"
     $('header').text('New Header!!!');
   });
